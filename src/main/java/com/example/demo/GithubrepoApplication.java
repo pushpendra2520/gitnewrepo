e1a2824499feaacdc64192bef418d50ab7cc0b8c
pushpendra <pushpendrasen2520@gmail.com>
2021-12-21 18:24:52 +0530

    git new repo done

---
+++ src/main/java/com/example/demo/GithubrepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubrepoApplication.class, args);
+	}
+
+}
